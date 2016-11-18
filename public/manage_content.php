a5f1071beee13317017835ca7e500540cc0d3b39
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-18 11:50:11 +0100

    [Project - Widget Corp] Moving the navigation to functions.php

--- public/manage_content.php
+++ public/manage_content.php
@@ -20,44 +20,7 @@
 <!-- Main content -->
 <div id="main">
 	<div id="navigation">
-		<ul class="subjects">
-			<?php $subject_set = find_all_subjects(); ?>
-			<?php
-				while($subject = mysqli_fetch_assoc($subject_set)) {
-			?>
-				<?php
-					echo "<li";
-					if ($subject["id"] == $selected_subject_id) {
-						echo " class=\"selected\"";
-					}
-					echo ">";
-				?>
-					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
-					<?php $page_set = find_pages_for_subject($subject['id']) ?>
-					<ul class="pages">
-						<?php
-							while($page = mysqli_fetch_assoc($page_set)) {
-						?>
-							<?php
-								echo "<li";
-								if ($page["id"] == $selected_page_id) {
-									echo " class=\"selected\"";
-								}
-								echo ">";
-							?><a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a></li>
-						<?php
-							}
-						?>
-					</ul>
-				</li>
-			<?php
-				}
-			?>
-			<!-- Release returned data -->
-			<?php
-				mysqli_free_result($subject_set);
-			?>
-		</ul>
+		<?php echo navigation($selected_subject_id, $selected_page_id); ?>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
