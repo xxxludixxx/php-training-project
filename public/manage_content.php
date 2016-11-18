3d9922cbd2d3f2db2a7771c1fd370ea976915b4c
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-18 11:48:10 +0100

    [Project - Widget Corp] Adding pages to the navigation subjects

--- public/manage_content.php
+++ public/manage_content.php
@@ -2,29 +2,52 @@
 <?php require_once ("../includes/db_connection.php"); ?>
 <!-- PHP Functions -->
 <?php require_once("../includes/functions.php"); ?>
-<!-- Database query -->
-<?php
-	$query  = "SELECT * ";
-	$query .= "FROM subjects ";
-	$query .= "WHERE visible = 1 ";
-	$query .= "ORDER BY position ASC";
-	$result = mysqli_query($connection, $query);
-	confirm_query($result);
-?>
 <!-- Header -->
 <?php include("../includes/layouts/header.php"); ?>
 <!-- Main content -->
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
+			<!-- Database query for subjects -->
 			<?php
-				// Use returned data to create navigation
-				while($subject = mysqli_fetch_assoc($result)) {
-					// output data from each row
-					?>
-					<li><?php echo $subject["menu_name"]?></li>
-					<?php
+				$query  = "SELECT * ";
+				$query .= "FROM subjects ";
+				$query .= "WHERE visible = 1 ";
+				$query .= "ORDER BY position ASC";
+				$subject_set = mysqli_query($connection, $query);
+				confirm_query($subject_set);
+			?>
+			<?php
+				while($subject = mysqli_fetch_assoc($subject_set)) {
+			?>
+					<li>
+						<?php echo $subject["menu_name"]; ?>
+						<!-- Database query for pages -->
+						<?php
+							$query  = "SELECT * ";
+							$query .= "FROM pages ";
+							$query .= "WHERE visible = 1 ";
+							$query .= "AND subject_id = {$subject['id']} ";
+							$query .= "ORDER BY position ASC";
+							$page_set = mysqli_query($connection, $query);
+							confirm_query($page_set);
+						?>
+						<ul class="pages">
+							<?php
+								while($page = mysqli_fetch_assoc($page_set)) {
+							?>
+							<li><?php echo $page["menu_name"]; ?></li>
+							<?php
+								}
+							?>
+						</ul>
+					</li>
+			<?php
 				}
+			?>
+			<!-- Release returned data -->
+			<?php
+				mysqli_free_result($subject_set);
 			?>
 		</ul>
 	</div>
@@ -32,9 +55,5 @@
 		<h2>Manage Content</h2>
 	</div>
 </div>
-<!-- Release returned data -->
-<?php
-	mysqli_free_result($result);
-?>
 <!-- Footer -->
 <?php include("../includes/layouts/footer.php"); ?>
