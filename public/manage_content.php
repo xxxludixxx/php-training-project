c429a7fae27bbfc9058ac191f00d7e6ad270c327
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-18 11:49:40 +0100

    [Project - Widget Corp] Adding styling to selected navigation elements

--- public/manage_content.php
+++ public/manage_content.php
@@ -25,14 +25,26 @@
 			<?php
 				while($subject = mysqli_fetch_assoc($subject_set)) {
 			?>
-				<li>
+				<?php
+					echo "<li";
+					if ($subject["id"] == $selected_subject_id) {
+						echo " class=\"selected\"";
+					}
+					echo ">";
+				?>
 					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
 					<?php $page_set = find_pages_for_subject($subject['id']) ?>
 					<ul class="pages">
 						<?php
 							while($page = mysqli_fetch_assoc($page_set)) {
 						?>
-						<li><a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a></li>
+							<?php
+								echo "<li";
+								if ($page["id"] == $selected_page_id) {
+									echo " class=\"selected\"";
+								}
+								echo ">";
+							?><a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a></li>
 						<?php
 							}
 						?>
