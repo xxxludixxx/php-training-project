508b600d564e6c5e107b55e3c787e7b9b2da3822
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-18 11:49:12 +0100

    [Project - Widget Corp] Making navigation elements URLs

--- public/manage_content.php
+++ public/manage_content.php
@@ -4,6 +4,19 @@
 <?php require_once("../includes/functions.php"); ?>
 <!-- Header -->
 <?php include("../includes/layouts/header.php"); ?>
+<?php
+	if(isset($_GET["subject"])) {
+		$selected_subject_id = $_GET["subject"];
+		$selected_page_id = null;
+	} elseif (isset($_GET["page"])) {
+		$selected_page_id = $_GET["page"];
+		$selected_subject_id = null;
+	} else {
+		$selected_subject_id = null;
+		$selected_page_id = null;
+	}
+?>
+
 <!-- Main content -->
 <div id="main">
 	<div id="navigation">
@@ -12,19 +25,19 @@
 			<?php
 				while($subject = mysqli_fetch_assoc($subject_set)) {
 			?>
-					<li>
-						<?php echo $subject["menu_name"]; ?>
-						<?php $page_set = find_pages_for_subject($subject['id']) ?>
-						<ul class="pages">
-							<?php
-								while($page = mysqli_fetch_assoc($page_set)) {
-							?>
-							<li><?php echo $page["menu_name"]; ?></li>
-							<?php
-								}
-							?>
-						</ul>
-					</li>
+				<li>
+					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
+					<?php $page_set = find_pages_for_subject($subject['id']) ?>
+					<ul class="pages">
+						<?php
+							while($page = mysqli_fetch_assoc($page_set)) {
+						?>
+						<li><a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a></li>
+						<?php
+							}
+						?>
+					</ul>
+				</li>
 			<?php
 				}
 			?>
@@ -36,6 +49,8 @@
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
+		<?php echo $selected_subject_id; ?>
+		<?php echo $selected_page_id; ?>
 	</div>
 </div>
 <!-- Footer -->
