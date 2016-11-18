3a29a57214ba9c079c7439be3dd01f5d4da2704e
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-18 11:51:41 +0100

    [Project - Widget Corp] Creating new_subject.php page

--- public/manage_content.php
+++ public/manage_content.php
@@ -10,6 +10,8 @@
 <div id="main">
 	<div id="navigation">
 		<?php echo navigation($current_subject, $current_page); ?>
+        <br />
+        <a href="new_subject.php">+ Add a subject</a>
 	</div>
 	<div id="page">
 		<?php if ($current_subject) { ?>
