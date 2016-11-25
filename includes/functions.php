33abb34eb9b93f4d5d09628cc9e46ab9fa3a035f
Jakub Ludwa <jludwa@creatuity.pl>
2016-11-25 11:28:46 +0100

    [Project - Widget Corp] Adding Admin management pages and functionality

--- includes/functions.php
+++ includes/functions.php
@@ -208,4 +208,33 @@
         $output .= "</ul>";
         return $output;
     }
+
+    function find_all_admins() {
+        global $connection;
+
+        $query = "SELECT * ";
+        $query .= "FROM admins ";
+        $query .= "ORDER BY username ASC";
+        $admin_set = mysqli_query($connection, $query);
+        confirm_query($admin_set);
+        return $admin_set;
+    }
+
+    function find_admin_by_id($admin_id) {
+        global $connection;
+
+        $safe_admin_id = mysqli_real_escape_string($connection, $admin_id);
+
+        $query = "SELECT * ";
+        $query .= "FROM admins ";
+        $query .= "WHERE id = {$safe_admin_id} ";
+        $query .= "LIMIT 1";
+        $admin_set = mysqli_query($connection, $query);
+        confirm_query($admin_set);
+        if($admin = mysqli_fetch_assoc($admin_set)) {
+            return $admin;
+        } else {
+            return null;
+        }
+    }
 ?>
